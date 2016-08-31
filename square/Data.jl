e1b8a6e4e2e51118edea073b34eb0563171bfc75
Praveen C <cpraveen@gmail.com>
2016-08-31 09:25:03 +0530

    Added some comments

--- square/Data.jl
+++ square/Data.jl
@@ -1,15 +1,16 @@
 module Data
 
-# This returns a scalar
+# Neumann bc, this returns a scalar
 function g(u)
    return 0.0;
 end
 
+# Dirichlet bc
 function u_d(u)
   return zeros( size( u, 1 ), 1 );
 end
 
-# This returns a scalar
+# RHS function in poisson equation. This returns a scalar
 function f(u)
    return 2.0*pi*pi*sin(pi*u[1])*sin(pi*u[2]);
 end
