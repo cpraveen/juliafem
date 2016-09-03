86476af9d782de9de400261649117ff17396403a
Praveen C <cpraveen@gmail.com>
2016-09-03 11:57:49 +0530

    Remove semicolon at line end as this is not needed in julia

--- src/fem.jl
+++ src/fem.jl
@@ -4,9 +4,9 @@ using Data
 using PyPlot
 
 function stima3(vertices)
-   d = size(vertices,2);
-   G = [ ones(1,d+1); vertices' ] \ [ zeros(1,d); eye(d) ];
-   return det([ones(1,d+1); vertices']) * G * G' / prod(1:d);
+   d = size(vertices,2)
+   G = [ ones(1,d+1); vertices' ] \ [ zeros(1,d); eye(d) ]
+   return det([ones(1,d+1); vertices']) * G * G' / prod(1:d)
 end
 
 function stima4(vertices)
@@ -22,44 +22,44 @@ function stima4(vertices)
 end
 
 function fem_50()
-   coord = readdlm("coordinates.dat");
-   elem3 = isfile("elements3.dat") ? round(Int64,readdlm("elements3.dat")) : [];
-   elem4 = isfile("elements4.dat") ? round(Int64,readdlm("elements4.dat")) : [];
-   neumann = isfile("neumann.dat") ? round(Int64,readdlm("neumann.dat")) : [];
-   dirichlet = round(Int64,readdlm("dirichlet.dat"));
-   n = size(coord,1); nt = size(elem3,1); nq = size(elem4,1);
+   coord = readdlm("coordinates.dat")
+   elem3 = isfile("elements3.dat") ? round(Int64,readdlm("elements3.dat")) : []
+   elem4 = isfile("elements4.dat") ? round(Int64,readdlm("elements4.dat")) : []
+   neumann = isfile("neumann.dat") ? round(Int64,readdlm("neumann.dat")) : []
+   dirichlet = round(Int64,readdlm("dirichlet.dat"))
+   n, nt, nq = size(coord,1), size(elem3,1), size(elem4,1)
    println("Number of vertices, triangles, quads = $n, $nt, $nq")
 
-   A = spzeros(n,n);
-   b = zeros(n);
+   A = spzeros(n,n)
+   b = zeros(n)
 
    for j in 1:nt
-      v = vec(elem3[j,:]);
-      A[v,v] += stima3(coord[v,:]);
-      b[v] += det([[1,1,1]'; coord[v,:]']) * f(sum(coord[v,:],1)/3)/6;
+      v = vec(elem3[j,:])
+      A[v,v] += stima3(coord[v,:])
+      b[v] += det([[1,1,1]'; coord[v,:]']) * f(sum(coord[v,:],1)/3)/6
    end
 
    for j in 1:nq
-      v = vec(elem4[j,:]);
-      A[v,v] += stima4(coord[v,:]);
-      b[v] += det([[1,1,1]'; coord[v[1:3],:]']) * f(sum(coord[v,:],1)/4)/4;
+      v = vec(elem4[j,:])
+      A[v,v] += stima4(coord[v,:])
+      b[v] += det([[1,1,1]'; coord[v[1:3],:]']) * f(sum(coord[v,:],1)/4)/4
    end
 
    if ~isempty(neumann)
-      nn = size(neumann,1);
+      nn = size(neumann,1)
       for j in 1:nn
-         v = vec(neumann[j,:]);
-         b[v] += norm(coord[v[1],:]-coord[v[2],:]) * g(sum(coord[v,:],1)/2)/2;
+         v = vec(neumann[j,:])
+         b[v] += norm(coord[v[1],:]-coord[v[2],:]) * g(sum(coord[v,:],1)/2)/2
       end
    end
 
-   u = zeros(n);
-   BoundNodes = unique(dirichlet);
-   u[BoundNodes] = u_d( coord[BoundNodes,:] );
-   b -= A * u;
+   u = zeros(n)
+   BoundNodes = unique(dirichlet)
+   u[BoundNodes] = u_d( coord[BoundNodes,:] )
+   b -= A * u
 
-   FreeNodes = setdiff( 1:n, BoundNodes );
-   u[FreeNodes] = A[FreeNodes,FreeNodes] \ b[FreeNodes];
+   FreeNodes = setdiff( 1:n, BoundNodes )
+   u[FreeNodes] = A[FreeNodes,FreeNodes] \ b[FreeNodes]
 
    triplot(coord[:,1], coord[:,2], elem3-1, color=(0.0,0.25,0.15),linewidth=0.2)
    tricontour(coord[:,1], coord[:,2], elem3-1, u, linewidth=2)
